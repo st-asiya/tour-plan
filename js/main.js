bc1fc8d53571dcb3c0cdeddb464ab0b837c5bbc6
st-asiya <kiki.go@mail.ru>
2021-05-23 13:11:04 +0300

    create: add review and fix hotel slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const swiper = new Swiper(".swiper-container", {
+const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   
   // Add effect
@@ -15,5 +15,17 @@ const swiper = new Swiper(".swiper-container", {
   keyboard: {
     pageUpDown: true,
   },
+
+});
+
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  }, 
 
 });
