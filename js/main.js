80e6aaac17e195fab05286ac7cbf227636175878
st-asiya <kiki.go@mail.ru>
2021-05-24 15:19:33 +0300

    add: parallax for newsletter

--- js/main.js
+++ js/main.js
@@ -29,3 +29,27 @@ const reviewsSlider = new Swiper(".reviews-slider", {
   }, 
 
 });
+
+
+
+
+$(window).scroll(function(e) {
+  parallax();
+})
+
+
+function parallax() {
+  var scroll = $(window).scrollTop();
+  var screenHeight = $(window).height();
+
+  $('.parallax').each(function() {
+    var offset = $(this).offset().top;
+    var distanceFromBottom = offset - scroll - screenHeight
+    
+    if (offset > screenHeight && offset) {
+      $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.06) +'px');
+    } else {
+      $(this).css('background-position', 'center ' + (( -scroll ) * 1) + 'px');
+    }
+  })
+}
